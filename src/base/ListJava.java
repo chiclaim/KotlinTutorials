f8ec1a8eb75000b7a7550615f5e60856e53b7f9a
kumu <kumu@2dfire.com>
2018-09-18 19:49:35 +0800

    kotlin集合；kotlin集合和Java集合交互
    Signed-off-by: kumu <kumu@2dfire.com>

---
+++ src/base/ListJava.java
@@ -0,0 +1,14 @@
+package base;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Desc:
+ * Created by Chiclaim on 2018/9/18.
+ */
+public class ListJava {
+    public static  List<Integer> getList() {
+        return Arrays.asList(null, 1, 2, 3);
+    }
+}
