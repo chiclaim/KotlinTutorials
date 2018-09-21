6abaf45749d3223eed40bfc0aaf43c536f167493
kumu <kumu@2dfire.com>
2018-09-21 17:34:30 +0800

    kotlin member 修饰符

--- other_module/src/visibility_modifier/MemberModifierTest.kt
+++ other_module/src/visibility_modifier/MemberModifierTest.kt
@@ -6,14 +6,20 @@ import visibility_modifier.modifier_member.MemberModifier
  * Desc:
  * Created by Chiclaim on 2018/9/21.
  */
+
+//在不同模块测试访问修饰符
 class MemberModifierTest {
 
-    val memberModifier: MemberModifier = MemberModifier()
+    private val memberModifier: MemberModifier = MemberModifier()
 
 
     fun call() {
-        memberModifier.username
+        println("${memberModifier.username}")
     }
 
 
+}
+
+fun main(args: Array<String>) {
+    MemberModifierTest().call()
 }
