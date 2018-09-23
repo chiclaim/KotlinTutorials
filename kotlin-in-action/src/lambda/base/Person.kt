fedeea7041e2468df6973f73747bc700b681d7b7
chiclaim <chiclaim@gmail.com>
2018-09-23 23:04:21 +0800

    add person list init
    Signed-off-by: chiclaim <chiclaim@gmail.com>

--- kotlin-in-action/src/lambda/base/Person.kt
+++ kotlin-in-action/src/lambda/base/Person.kt
@@ -3,3 +3,8 @@ package lambda.base
 data class Person(var name: String, var age: Int) {
     lateinit var children: List<Person>
 }
+
+val list = listOf<Person>(Person("chiclaim", 18), Person("yuzhiqiang", 15),
+        Person("johnny", 27), Person("clause", 190),
+        Person("fin", 190))
+
