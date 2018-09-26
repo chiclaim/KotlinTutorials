a1a1c82948d734a3f0531af457a43f8993afe1e5
kumu <kumu@2dfire.com>
2018-09-26 10:58:23 +0800

    lambda receiver 修改描述

--- kotlin-in-action/src/lambda/base/LambdaWithReceiver.kt
+++ kotlin-in-action/src/lambda/base/LambdaWithReceiver.kt
@@ -8,6 +8,7 @@ package lambda.base
 
 //with函数有两个参数，一个是receiver 一个是lambda表达式。传递lambda参数，一般放在外面
 //此时lambda receiver就是with的第一个参数
+//使用with函数，调用receiver对象的方法时，可以很的省略这个对象，直接调用。如下面的append方法
 fun alphabet() = with(StringBuilder()) {
     for (letter in 'A'..'Z') {
         append(letter)
@@ -19,7 +20,7 @@ fun alphabet() = with(StringBuilder()) {
 
 //apply函数是一个扩展函数
 //此时lambda receiver是调用apply函数的对象
-//使用apply函数可以很方便地对某个对象进行初始化操作
+//使用apply函数可以创建对象后很方便地进行初始化操作
 fun alphabet2() = StringBuilder().apply {
     for (letter in 'A'..'Z') {
         append(letter)
