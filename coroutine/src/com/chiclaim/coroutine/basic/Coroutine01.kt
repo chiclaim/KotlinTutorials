c21995d15e3084e50276dcd8a7965f668034a56e
kumu <kumu@2dfire.com>
2019-01-11 14:03:13 +0800

    [add] kotlin coroutine

---
+++ coroutine/src/com/chiclaim/coroutine/basic/Coroutine01.kt
@@ -0,0 +1,12 @@
+package com.chiclaim.coroutine.basic
+
+import kotlinx.coroutines.*
+
+fun main() {
+    GlobalScope.launch { // launch new coroutine in background and continue
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    println("Hello,") // main thread continues while coroutine is delayed
+    Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+}
