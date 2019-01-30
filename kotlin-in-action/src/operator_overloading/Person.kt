cb77cfeb980577c1377f622024e1182606a4ed92
chiclaim <chiclaim@gmail.com>
2019-01-30 20:02:08 +0800

    [update] 添加equals方法实现

--- kotlin-in-action/src/operator_overloading/Person.kt
+++ kotlin-in-action/src/operator_overloading/Person.kt
@@ -10,4 +10,9 @@ class Person(val name: String, val age: Int) : Comparable<Person> {
         //比较字段是可变参数，可以输入多个
         return compareValuesBy(this, other, Person::age, Person::name)
     }
+
+    override fun equals(other: Any?): Boolean {
+        val o = other as? Person
+        return name == o?.name
+    }
 }
