766f78eac8e458a5e9bec1579ad8e1effe7479be
chiclaim <chiclaim@gmail.com>
2019-03-03 14:40:23 +0800

    [add] when 表达式

--- kotlin-in-action/src/base/WhenTest.kt
+++ kotlin-in-action/src/base/WhenTest.kt
@@ -1,5 +1,7 @@
 package base
+
 import  new_class.Color
+import  new_class.Color.*
 
 
 // when with enum
@@ -14,5 +16,27 @@ fun getMnemonic(color: Color) = when (color) {
 }
 
 
+fun mix(c1: Color, c2: Color) = when (setOf(c1, c2)) {
+    setOf(RED, YELLOW) -> ORANGE
+    setOf(YELLOW, BLUE) -> GREEN
+    setOf(BLUE, VIOLET) -> INDIGO
+    else -> throw Exception("Dirty color")
+}
 
 
+fun mixOptimized(c1: Color, c2: Color) = when {
+    (c1 == RED && c2 == YELLOW) || (c1 == YELLOW && c2 == RED) ->
+        ORANGE
+    (c1 == YELLOW && c2 == BLUE) || (c1 == BLUE && c2 == YELLOW) ->
+        GREEN
+    (c1 == BLUE && c2 == VIOLET) || (c1 == VIOLET && c2 == BLUE) ->
+        INDIGO
+    else -> throw Exception("Dirty color")
+}
+
+internal fun test(obj: Any) {
+    when (obj) {
+        is String -> obj.substring(0, obj.length / 2)
+    }
+
+}
