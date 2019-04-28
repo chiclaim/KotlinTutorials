7f52ebfb886b6b2fa91bbf88e05db48eff9f032e
chiclaim <chiclaim@gmail.com>
2019-04-28 16:36:11 +0800

    [update] change lambda receiver

--- kotlin-in-action/src/higher_order_function/LambdaReceiverChangeTest.kt
+++ kotlin-in-action/src/higher_order_function/LambdaReceiverChangeTest.kt
@@ -4,6 +4,12 @@ fun main() {
 
     val any = Any()
     println(any)
+    // 一般来说 lambda receiver 是调用该高阶函数对象
+    any.apply {
+        println(this)
+    }
+
+    // lambda receiver 并不是 `any` 对象，lambda receiver 已经被改变(apply3)
     val result = any.apply2 {
         println(this)  // The obj of this  has changed
     }
