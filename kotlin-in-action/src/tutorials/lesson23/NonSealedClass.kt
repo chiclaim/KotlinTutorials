eb553132b4d65d2dd313a6c0c2b6fbfb9e0f9aba
chiclaim <chiclaim@gmail.com>
2022-06-04 10:59:36 +0800

    update

--- kotlin-in-action/src/tutorials/lesson23/NonSealedClass.kt
+++ kotlin-in-action/src/tutorials/lesson23/NonSealedClass.kt
@@ -16,6 +16,17 @@ fun testWhenClass(gender: Gender) {
     }
 }
 
+/*
+如果我们打成 jar 给其他地方使用，如果外部继承了 Gender，然后将新的类型传递给 testWhenClass2，
+那么我们封装的类就会有问题，因为一开始我们以为只会有 Male、Female
+ */
+fun testWhenClass2(gender: Gender) {
+    when (gender) {
+        is Gender.Male -> println("Male")
+        else -> println("Female")
+    }
+}
+
 fun main() {
     testWhenClass(Gender.Male())
 }
