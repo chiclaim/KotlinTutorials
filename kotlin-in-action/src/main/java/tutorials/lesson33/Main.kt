a521f8837fc05019589ad53891aebbaee6e47b58
chiclaim <chiclaim@gmail.com>
2023-09-01 18:14:18 +0800

    lesson33 return/continue/break

---
+++ kotlin-in-action/src/main/java/tutorials/lesson33/Main.kt
@@ -0,0 +1,64 @@
+package tutorials.lesson33
+
+
+fun testForContinue() {
+    for (i in 1..10) {
+        if (i % 2 == 0) continue
+        println(i)
+    }
+    println("end...")
+}
+
+fun testForBreak() {
+    for (i in 1..10) {
+        if (i % 2 == 0) break
+        println(i)
+    }
+    println("end...")
+}
+
+fun testNestForBreak() {
+    loop@ for (i in 1..10) {
+        for (j in 1..10) {
+            if (i % 2 == 0) break@loop
+        }
+        println(i)
+    }
+    println("end...")
+}
+
+fun testForReturn() {
+    for (i in 1..10) {
+        if (i % 2 == 0) return
+        println(i)
+    }
+    println("end...")
+}
+
+fun testForeachContinue() {
+    val list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    list.forEach { i ->
+        // if (i % 2 == 0) continue
+        if (i % 2 == 0) return@forEach
+        println(i)
+    }
+    println("end...")
+}
+
+
+fun testForeachBreak() {
+    val list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    run loop@{
+        list.forEach { i ->
+            // if (i % 2 == 0) break
+            if (i % 2 == 0) return@loop
+            println(i)
+        }
+    }
+    println("end...")
+
+}
+
+fun main() {
+    testForeachBreak()
+}
